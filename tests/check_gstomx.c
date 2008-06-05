06d3f9b4e8ca9d5c33fcb94ebc804ea2e0e988a7
Felipe Contreras <felipe.contreras@nokia.com>
2008-06-05 16:20:06 +0000

    Add flush unit test.
    Also make the tests more GStreamish.

--- tests/check_gstomx.c
+++ tests/check_gstomx.c
@@ -21,28 +21,19 @@
 
 #include <gst/check/gstcheck.h>
 
-typedef struct Core Core;
-
-struct Core
-{
-    GstElement *pipeline;
-    GMainLoop *loop;
-};
+#define BUFFER_SIZE 0x1000
+#define BUFFER_COUNT 0x100
+#define FLUSH_AT 0x10
 
 static gboolean
 bus_cb (GstBus *bus,
         GstMessage *msg,
         gpointer data)
 {
-    Core *core;
-
-    core = data;
-
     switch (GST_MESSAGE_TYPE (msg))
     {
         case GST_MESSAGE_EOS:
             /* g_debug ("end-of-stream"); */
-            g_main_loop_quit (core->loop);
             break;
         case GST_MESSAGE_ERROR:
             {
@@ -64,74 +55,116 @@ bus_cb (GstBus *bus,
     return TRUE;
 }
 
-static Core *
-core_new (void)
-{
-    Core *core;
-    core = g_new0 (Core, 1);
+static GstStaticPadTemplate sinktemplate =
+GST_STATIC_PAD_TEMPLATE ("sink",
+                         GST_PAD_SINK,
+                         GST_PAD_ALWAYS,
+                         GST_STATIC_CAPS_ANY);
 
-    core->loop = g_main_loop_new (NULL, FALSE);
-
-    return core;
-}
+static GstStaticPadTemplate srctemplate =
+GST_STATIC_PAD_TEMPLATE ("src",
+                         GST_PAD_SRC,
+                         GST_PAD_ALWAYS,
+                         GST_STATIC_CAPS_ANY);
 
 static void
-core_free (Core *core)
+helper (gboolean flush)
 {
-    gst_element_set_state (core->pipeline, GST_STATE_NULL);
-    gst_object_unref (GST_OBJECT (core->pipeline));
+    GstElement *filter;
+    GstBus *bus;
+    GstPad *mysrcpad;
+    GstPad *mysinkpad;
 
-    g_main_loop_unref (core->loop);
+    /* init */
+    filter = gst_check_setup_element ("omx_dummy");
+    mysrcpad = gst_check_setup_src_pad (filter, &srctemplate, NULL);
+    mysinkpad = gst_check_setup_sink_pad (filter, &sinktemplate, NULL);
 
-    g_free (core);
+    gst_pad_set_active (mysrcpad, TRUE);
+    gst_pad_set_active (mysinkpad, TRUE);
+
+    g_object_set (G_OBJECT (filter), "library-name", "libomxil-foo.so", NULL);
+
+    /* start */
+
+    fail_unless_equals_int (gst_element_set_state (filter, GST_STATE_PLAYING),
+                            GST_STATE_CHANGE_SUCCESS);
+
+    bus = gst_bus_new ();
+
+    gst_element_set_bus (filter, bus);
+
+    /* send buffers in order*/
+    {
+        guint i;
+        for (i = 0; i < BUFFER_COUNT; i++)
+        {
+            GstBuffer *inbuffer;
+            inbuffer = gst_buffer_new_and_alloc (BUFFER_SIZE);
+            GST_BUFFER_DATA(inbuffer)[0] = i;
+            ASSERT_BUFFER_REFCOUNT (inbuffer, "inbuffer", 1);
+
+            fail_unless (gst_pad_push (mysrcpad, inbuffer) == GST_FLOW_OK);
+
+            if (flush && i % FLUSH_AT == 0)
+            {
+                gst_pad_push_event (mysrcpad, gst_event_new_flush_start ());
+                gst_pad_push_event (mysrcpad, gst_event_new_flush_stop ());
+                i += FLUSH_AT;
+            }
+        }
+    }
+
+    {
+        GstMessage *message;
+        fail_if ((message = gst_bus_pop (bus)) != NULL);
+
+        /* make sure there's no error on the bus */
+        message = gst_bus_poll (bus, GST_MESSAGE_ERROR, 0);
+        fail_if (message);
+    }
+
+    gst_pad_push_event (mysrcpad, gst_event_new_eos ());
+
+    /* check the order of the buffers*/
+    if (!flush)
+    {
+        GList *cur;
+        guint i;
+        for (cur = buffers, i = 0; cur; cur = g_list_next (cur), i++)
+        {
+            GstBuffer *buffer;
+            buffer = cur->data;
+            fail_unless (GST_BUFFER_DATA(buffer)[0] == i);
+        }
+        fail_unless (i == BUFFER_COUNT);
+    }
+
+    /* cleanup */
+    gst_bus_set_flushing (bus, TRUE);
+    gst_element_set_bus (filter, NULL);
+    gst_object_unref (GST_OBJECT (bus));
+    gst_check_drop_buffers ();
+
+    /* deinit */
+    gst_element_set_state (filter, GST_STATE_NULL);
+
+    gst_pad_set_active (mysrcpad, FALSE);
+    gst_pad_set_active (mysinkpad, FALSE);
+    gst_check_teardown_src_pad (filter);
+    gst_check_teardown_sink_pad (filter);
+    gst_check_teardown_element (filter);
 }
+
+GST_START_TEST (test_flush)
+{
+    helper (TRUE);
+}
+GST_END_TEST
 
 GST_START_TEST (test_basic)
 {
-    Core *core;
-
-    gst_init (NULL, NULL);
-
-    core = core_new ();
-
-    core->pipeline = gst_pipeline_new ("audio-player");
-    fail_if (!core->pipeline);
-
-    {
-        GstElement *src;
-        GstElement *filter;
-        GstElement *sink;
-
-        src = gst_element_factory_make ("filesrc", "src");
-        fail_if (!src);
-        g_object_set (G_OBJECT (src), "location", "/tmp/test.mp3", NULL);
-        gst_bin_add (GST_BIN (core->pipeline), src);
-
-        filter = gst_element_factory_make ("omx_dummy", "dummy");
-        fail_if (!filter);
-        g_object_set (G_OBJECT (filter), "library-name", "libomxil-foo.so", NULL);
-        gst_bin_add (GST_BIN (core->pipeline), filter);
-        gst_element_link (src, filter);
-
-        sink = gst_element_factory_make ("filesink", "sink");
-        fail_if (!sink);
-        g_object_set (G_OBJECT (sink), "location", "/tmp/test_out.mp3", NULL);
-        gst_bin_add (GST_BIN (core->pipeline), sink);
-        gst_element_link (filter, sink);
-    }
-
-    {
-        GstBus *bus;
-        bus = gst_pipeline_get_bus (GST_PIPELINE (core->pipeline));
-        gst_bus_add_watch (bus, bus_cb, core);
-        gst_object_unref (bus);
-    }
-
-    gst_element_set_state (core->pipeline, GST_STATE_PLAYING);
-
-    g_main_loop_run (core->loop);
-
-    core_free (core);
+    helper (FALSE);
 }
 GST_END_TEST
 
@@ -143,6 +176,7 @@ gstomx_suite (void)
 
   tcase_set_timeout (tc_chain, 10);
   tcase_add_test (tc_chain, test_basic);
+  tcase_add_test (tc_chain, test_flush);
   suite_add_tcase (s, tc_chain);
 
   return s;
