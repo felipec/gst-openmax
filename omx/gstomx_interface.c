50a760c15bd6bb806c7df29667defaf1aa38c0be
Felipe Contreras <felipe.contreras@nokia.com>
2008-08-15 15:07:04 +0000

    New omx interface.

---
+++ omx/gstomx_interface.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2008 Nokia Corporation.
+ *
+ * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "gstomx_interface.h"
+
+GType
+gst_omx_get_type (void)
+{
+    static GType type = 0;
+
+    if (G_UNLIKELY (type == 0))
+    {
+        GTypeInfo *type_info;
+
+        type_info = g_new0 (GTypeInfo, 1);
+
+        type_info->class_size = sizeof (GstOmxClass);
+
+        type = g_type_register_static (G_TYPE_INTERFACE, "GstOmx", type_info, 0);
+        g_type_interface_add_prerequisite (type, GST_TYPE_IMPLEMENTS_INTERFACE);
+    }
+
+    return type;
+}
---
+++ omx/gstomx_interface.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2008 Nokia Corporation.
+ *
+ * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef GSTOMX_INTERFACE_H
+#define GSTOMX_INTERFACE_H
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_OMX (gst_omx_get_type ())
+#define GST_OMX(obj) (GST_IMPLEMENTS_INTERFACE_CHECK_INSTANCE_CAST ((obj), GST_TYPE_OMX, GstOmx))
+#define GST_IS_OMX(obj) (GST_IMPLEMENTS_INTERFACE_CHECK_INSTANCE_TYPE ((obj), GST_TYPE_OMX))
+
+typedef struct GstOmx GstOmx;
+
+typedef struct GstOmxClass
+{
+    GTypeInterface klass;
+
+} GstOmxClass;
+
+GType gst_omx_get_type (void);
+
+G_END_DECLS
+
+#endif /* GSTOMX_INTERFACE_H */
