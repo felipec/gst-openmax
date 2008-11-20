24464fc76ed884be2d63e81f078e06a3b4d1a106
Felipe Contreras <felipe.contreras@nokia.com>
2008-11-20 16:59:56 +0000

    Add new semaphore utility.

---
+++ util/sem.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2008 Nokia Corporation.
+ *
+ * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <glib.h>
+
+#include "sem.h"
+
+GSem *
+g_sem_new (void)
+{
+    GSem *sem;
+
+    sem = g_new (GSem, 1);
+    sem->condition = g_cond_new ();
+    sem->mutex = g_mutex_new ();
+    sem->counter = 0;
+
+    return sem;
+}
+
+void
+g_sem_free (GSem *sem)
+{
+    g_cond_free (sem->condition);
+    g_mutex_free (sem->mutex);
+    g_free (sem);
+}
+
+void
+g_sem_down (GSem *sem)
+{
+    g_mutex_lock (sem->mutex);
+
+    while (sem->counter == 0)
+    {
+        g_cond_wait (sem->condition, sem->mutex);
+    }
+
+    sem->counter--;
+
+    g_mutex_unlock (sem->mutex);
+}
+
+void
+g_sem_up (GSem *sem)
+{
+    g_mutex_lock (sem->mutex);
+
+    sem->counter++;
+    g_cond_signal (sem->condition);
+
+    g_mutex_unlock (sem->mutex);
+}
---
+++ util/sem.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2008 Nokia Corporation.
+ *
+ * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef SEM_H
+#define SEM_H
+
+#include <glib.h>
+
+typedef struct GSem GSem;
+
+struct GSem
+{
+    GCond *condition;
+    GMutex *mutex;
+    gint counter;
+};
+
+GSem *g_omx_sem_new (void);
+void g_omx_sem_free (GSem *sem);
+void g_omx_sem_down (GSem *sem);
+void g_omx_sem_up (GSem *sem);
+
+#endif /* SEM_H */
