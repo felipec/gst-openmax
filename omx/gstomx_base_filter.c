1c6bac7d44d72c12003159d729908b57d5c6536a
Mark Nauwelaerts <mark.nauwelaerts@collabora.co.uk>
2009-03-12 14:57:58 +0100

    Fix format specifier in debug statement.
    Signed-off-by: Mark Nauwelaerts <mark.nauwelaerts@collabora.co.uk>
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- omx/gstomx_base_filter.c
+++ omx/gstomx_base_filter.c
@@ -499,7 +499,7 @@ pad_chain (GstPad *pad,
     gomx = self->gomx;
 
     GST_LOG_OBJECT (self, "begin");
-    GST_LOG_OBJECT (self, "gst_buffer: size=%lu", GST_BUFFER_SIZE (buf));
+    GST_LOG_OBJECT (self, "gst_buffer: size=%u", GST_BUFFER_SIZE (buf));
 
     GST_LOG_OBJECT (self, "state: %d", gomx->omx_state);
 
