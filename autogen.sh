9359e10d922ca6ab4380f580a5b5a2f41ab83cea
Felipe Contreras <felipe.contreras@nokia.com>
2009-03-24 16:00:28 +0200

    Remove automatic pre-commit hook
    Clean commits should be enfored in a different way.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@nokia.com>

--- autogen.sh
+++ autogen.sh
@@ -22,13 +22,6 @@ fi
 
 mkdir -p config
 
-# install pre-commit hook for doing clean commits
-if test ! -x .git/hooks/pre-commit;
-then
-    cp common/hooks/pre-commit.hook .git/hooks/pre-commit
-    chmod +x .git/hooks/pre-commit
-fi
-
 CONFIGURE_DEF_OPT='--enable-maintainer-mode'
 
 autogen_options $@
