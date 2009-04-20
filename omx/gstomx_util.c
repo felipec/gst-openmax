c224f5929f716244274afa6eaab0b27920225c12
Felipe Contreras <felipe.contreras@gmail.com>
2009-04-21 00:32:53 +0300

    util: nicer error messages
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- omx/gstomx_util.c
+++ omx/gstomx_util.c
@@ -73,6 +73,9 @@ FillBufferDone (OMX_HANDLETYPE omx_handle,
                 OMX_PTR app_data,
                 OMX_BUFFERHEADERTYPE *omx_buffer);
 
+static inline const char *
+omx_error_to_str (OMX_ERRORTYPE omx_error);
+
 static inline GOmxPort *
 g_omx_core_get_port (GOmxCore *core,
                      guint index);
@@ -865,7 +868,8 @@ EventHandler (OMX_HANDLETYPE omx_handle,
         case OMX_EventError:
             {
                 core->omx_error = data_1;
-                GST_ERROR ("unrecoverable error: %lx", data_1);
+                GST_ERROR ("unrecoverable error: %s (0x%lx)",
+                           omx_error_to_str (data_1), data_1);
                 /* component might leave us waiting for buffers, unblock */
                 g_omx_core_flush_start (core);
                 /* unlock wait_for_state */
@@ -913,4 +917,128 @@ FillBufferDone (OMX_HANDLETYPE omx_handle,
     got_buffer (core, port, omx_buffer);
 
     return OMX_ErrorNone;
+}
+
+static inline const char *
+omx_error_to_str (OMX_ERRORTYPE omx_error)
+{
+    switch (omx_error)
+    {
+        case OMX_ErrorNone:
+            return "None";
+
+        case OMX_ErrorInsufficientResources:
+            return "There were insufficient resources to perform the requested operation";
+
+        case OMX_ErrorUndefined:
+            return "The cause of the error could not be determined";
+
+        case OMX_ErrorInvalidComponentName:
+            return "The component name string was not valid";
+
+        case OMX_ErrorComponentNotFound:
+            return "No component with the specified name string was found";
+
+        case OMX_ErrorInvalidComponent:
+            return "The component specified did not have an entry point";
+
+        case OMX_ErrorBadParameter:
+            return "One or more parameters were not valid";
+
+        case OMX_ErrorNotImplemented:
+            return "The requested function is not implemented";
+
+        case OMX_ErrorUnderflow:
+            return "The buffer was emptied before the next buffer was ready";
+
+        case OMX_ErrorOverflow:
+            return "The buffer was not available when it was needed";
+
+        case OMX_ErrorHardware:
+            return "The hardware failed to respond as expected";
+
+        case OMX_ErrorInvalidState:
+            return "The component is in invalid state";
+
+        case OMX_ErrorStreamCorrupt:
+            return "Stream is found to be corrupt";
+
+        case OMX_ErrorPortsNotCompatible:
+            return "Ports being connected are not compatible";
+
+        case OMX_ErrorResourcesLost:
+            return "Resources allocated to an idle component have been lost";
+
+        case OMX_ErrorNoMore:
+            return "No more indices can be enumerated";
+
+        case OMX_ErrorVersionMismatch:
+            return "The component detected a version mismatch";
+
+        case OMX_ErrorNotReady:
+            return "The component is not ready to return data at this time";
+
+        case OMX_ErrorTimeout:
+            return "There was a timeout that occurred";
+
+        case OMX_ErrorSameState:
+            return "This error occurs when trying to transition into the state you are already in";
+
+        case OMX_ErrorResourcesPreempted:
+            return "Resources allocated to an executing or paused component have been preempted";
+
+        case OMX_ErrorPortUnresponsiveDuringAllocation:
+            return "Waited an unusually long time for the supplier to allocate buffers";
+
+        case OMX_ErrorPortUnresponsiveDuringDeallocation:
+            return "Waited an unusually long time for the supplier to de-allocate buffers";
+
+        case OMX_ErrorPortUnresponsiveDuringStop:
+            return "Waited an unusually long time for the non-supplier to return a buffer during stop";
+
+        case OMX_ErrorIncorrectStateTransition:
+            return "Attempting a state transition that is not allowed";
+
+        case OMX_ErrorIncorrectStateOperation:
+            return "Attempting a command that is not allowed during the present state";
+
+        case OMX_ErrorUnsupportedSetting:
+            return "The values encapsulated in the parameter or config structure are not supported";
+
+        case OMX_ErrorUnsupportedIndex:
+            return "The parameter or config indicated by the given index is not supported";
+
+        case OMX_ErrorBadPortIndex:
+            return "The port index supplied is incorrect";
+
+        case OMX_ErrorPortUnpopulated:
+            return "The port has lost one or more of its buffers and it thus unpopulated";
+
+        case OMX_ErrorComponentSuspended:
+            return "Component suspended due to temporary loss of resources";
+
+        case OMX_ErrorDynamicResourcesUnavailable:
+            return "Component suspended due to an inability to acquire dynamic resources";
+
+        case OMX_ErrorMbErrorsInFrame:
+            return "Frame generated macroblock error";
+
+        case OMX_ErrorFormatNotDetected:
+            return "Cannot parse or determine the format of an input stream";
+
+        case OMX_ErrorContentPipeOpenFailed:
+            return "The content open operation failed";
+
+        case OMX_ErrorContentPipeCreationFailed:
+            return "The content creation operation failed";
+
+        case OMX_ErrorSeperateTablesUsed:
+            return "Separate table information is being used";
+
+        case OMX_ErrorTunnelingUnsupported:
+            return "Tunneling is unsupported by the component";
+
+        default:
+            return "Unknown error";
+    }
 }
